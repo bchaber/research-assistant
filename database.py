25dc5a834ea8b2187a0bc1fdfd06ece9379c1411
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2021-05-24 23:41:00 +0200

    Redis port should be int

--- database.py
+++ database.py
@@ -3,5 +3,4 @@ from redis import StrictRedis
 REDISHOST = os.getenv("REDISHOST")
 REDISPORT = os.getenv("REDISPORT")
 REDISAUTH = os.getenv("REDISAUTH")
-db  = StrictRedis(host=REDISHOST, port=REDISPORT, password=REDISAUTH, db=0)
-print("Connected to " + str(db))
+db  = StrictRedis(host=REDISHOST, port=int(REDISPORT), password=REDISAUTH, db=0)
