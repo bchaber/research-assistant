6a262ef1e03787d462988044c32ca71ddcca9c57
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2021-05-25 10:25:57 +0200

    Autodecode responses because we mostly use str not bytes

--- database.py
+++ database.py
@@ -3,4 +3,4 @@ from redis import StrictRedis
 REDISHOST = os.getenv("REDISHOST")
 REDISPORT = os.getenv("REDISPORT")
 REDISAUTH = os.getenv("REDISAUTH")
-db  = StrictRedis(host=REDISHOST, port=int(REDISPORT), password=REDISAUTH, db=0)
+db  = StrictRedis(host=REDISHOST, port=int(REDISPORT), password=REDISAUTH, db=0, decode_responses=True)
