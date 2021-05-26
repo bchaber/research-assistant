cbfb07b04785de409fe8206b9604340abdcca608
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2021-05-26 16:13:17 +0200

    Add a sci-hub tool

--- tools/scihub.py
+++ tools/scihub.py
@@ -1,3 +1,31 @@
 #!env python3
+from bs4 import BeautifulSoup 
+from requests import get
+
+SCIHUB_URL = "https://sci-hub.se/"
+
 def fetch_pdf(doi):
-  print("[?] fetch is not available yet")
+  response = get(SCIHUB_URL + doi)
+  if response.status_code != 200:
+    print("[!] Invalid response code " + response.status_code + ":")
+    print(response.content)
+    return None
+
+  soup = BeautifulSoup(response.content, "lxml")
+  iframe = soup.find("iframe", attrs={"id":"pdf"})
+  mirror = iframe["src"].split("#")[0]
+  if mirror.startswith("//"):
+    mirror = "https:" + mirror
+
+  document = get(mirror)
+  if document.headers["content-type"] != "application/pdf":
+    print("[!] Invalid content type: " + document.headers["content-type"])
+    return None
+
+  return document.content
+
+if __name__ == "__main__":
+  from sys import argv
+  content = fetch_pdf(argv[1])
+  if content:
+    print(content)
