3e8e31929b88b7f02ed74222eb5cd01ca2947f5c
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2021-05-26 11:09:55 +0200

    Add working dropbox_worker

--- dropbox_worker.py
+++ dropbox_worker.py
@@ -3,18 +3,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from sys import argv
 from rq import Queue, Worker, Connection
 from database import db
-queue = Queue(connection=db)
+name = 'default' if len(argv) < 2 else argv[1]
+queue = Queue(name, connection=db)
 
-def title(metadata):
-  return metadata.get("title", "")
-  
-def authors(metadata):
-  return ' and '.join([author.get("family") + ', ' + author.get("given")
-  	for author in metadata.get("author", [])[:3]])
-
-from tools import reader, finder
+from local_worker import title, authors
+from tools import reader, finder, scihub
 def parse_pdf_file(incoming):
   print("[*] processing new PDF file: " + incoming)
   doi = reader.extract_doi_from_file(incoming)
@@ -49,21 +45,24 @@ def parse_pdf_stream(incoming, content, dropbox):
 def move_pdf(incoming, outgoing, dropbox):
   print("[>] moving " + incoming + " to " + outgoing)
   dropbox.files_move(incoming, outgoing, autorename=True)
-  return outgoing
 
-def add_pdf(newname, content):
-  print("[x] would have add a PDF")
-  pass
-
-def fetch_pdf(doi):
-  print("[x] would have fetch a DOI")
-  pass
+def add_pdf(metadata, dropbox):
+  outgoing = '/tmp/outgoing/' + title(metadata) + " - " + authors(metadata) + ".pdf"
+  print("[v] adding " + outgoing)
+  doi = metadata.get("DOI")
+  if doi:
+    print("[v] fetching a PDF for " + doi)
+    content = scihub.fetch_pdf(doi)
+    #dropbox.files_upload(content, outgoing)
+    with open(outgoing, "wb") as f:
+      f.write(content)
 
 def add_bibitem(bibitem):
-  print("[x] would have add bibitem")
+  print("[x] should add the bibitem:")
+  print(bibitem)
   pass
 
-def find_citation(citation):
+def parse_citation(citation):
   print("[*] processing new citation:\n\t" + citation)
   doi = finder.find_doi(citation)
   if doi is None:
@@ -72,6 +71,7 @@ def find_citation(citation):
   if metadata is None:
     return "[err] error while finding metadata"
   queue.enqueue("__main__.add_bibitem", args=(bibitem,))
+  queue.enqueue("__main__.add_pdf", args=(metadata, 0))
 
 if __name__ == "__main__":
   with Connection(db):
